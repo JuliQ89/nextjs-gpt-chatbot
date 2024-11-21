20110c12a6aeed2c014d652da8bf07fb37aa931a
JuliQ89 <julian.singheiser.official@gmail.com>
2024-11-21 19:34:21 +0100

    updated§

--- backend/gptchatbot/chat/admin.py
+++ backend/gptchatbot/chat/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Chat, Message
 
-# Register your models here.
+class ChatAdminModel(admin.ModelAdmin):
+    search_fields = ['title']
+    list_display = ['title', 'created']
+
+class MessageAdminModel(admin.ModelAdmin):
+    search_fields = ['sender', 'created']
+    list_display = ['sender', 'sequence_number', 'created']
+
+admin.site.register(Chat, ChatAdminModel)
+admin.site.register(Message, MessageAdminModel)
--- backend/gptchatbot/chat/models.py
+++ backend/gptchatbot/chat/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+import uuid
 
-# Create your models here.
+
+class Chat(models.Model):
+    title = models.CharField(max_length=155)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    def __str__(self):
+        return self.title
+    
+
+class Message(models.Model):
+    class Sender(models.TextChoices):
+        USER = "USER"
+        BOT = "BOT"
+
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='messages')
+    sequence_number = models.IntegerField()
+    text = models.TextField()
+    sender = models.CharField(max_length=155, choices=Sender)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        last_message = Message.objects.last()
+        self.sequence_number = last_message.sequence_number if last_message.sequence_number else 0
+        return super().save(*args, **kwargs)
